debd3938bba1517636fb392cf46785f38fb91cd5
Maricris Villareal <ariscris@gmail.com>
2013-07-05 23:06:09 -0400

    fix

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	fs.readFile('/etc/passwd', function (err, data) {
+	var fs = require('fs');
+	fs.readFile('index.html', function (err, data) {
 	  if (err) throw err;
 	  response.send(data);
 	});	
